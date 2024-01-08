2762a65de2c421d6d0debcd29a2a406c54070a86
osparking <jbpark03@gmail.com>
2024-01-09 07:49:59 +0900

    자동 생성 시험 클래스

---
+++ src/test/java/com/gmail/jbpark03/beans/Ch17BootBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.jbpark03.beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch17BootBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
