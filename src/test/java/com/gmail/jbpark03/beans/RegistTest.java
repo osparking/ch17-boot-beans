c84a07b8dd78923185b24c0e3a8641d0ee310950
osparking <jbpark03@gmail.com>
2024-01-09 09:16:39 +0900

    시험용 빈 생성 클래스 시험클래스에 활용

---
+++ src/test/java/com/gmail/jbpark03/beans/RegistTest.java
@@ -0,0 +1,34 @@
+package com.gmail.jbpark03.beans;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+import com.gmail.jbpark03.beans.add.TestBeans;
+import com.gmail.jbpark03.beans.model.Passenger;
+import com.gmail.jbpark03.beans.regist.PassengerRegistEvent;
+import com.gmail.jbpark03.beans.regist.RegistManager;
+
+@SpringBootTest
+@ImportResource("classpath:application-context.xml")
+@Import(TestBeans.class)
+class RegistTest {
+
+  @Autowired
+  private RegistManager manager;
+
+  @Autowired
+  private Passenger passenger;
+
+  @Test
+  void test() {
+    var event = new PassengerRegistEvent(passenger);
+    System.out.println("사건 발생 후:" + passenger);
+    manager.getContext().publishEvent(event);
+    assertTrue(passenger.isRegistered());
+  }
+}
